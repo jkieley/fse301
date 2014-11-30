9751d227152548cc1abc03f56ddf41c1af501ab1
James Kieley <jkieley@asu.edu>
2014-11-30 15:10:13 -0700

    looking good

--- wp-content/themes/influence/demo/slider.php
+++ wp-content/themes/influence/demo/slider.php
@@ -10,12 +10,34 @@
 			</div>
 
 		</li>
+		<li class="sow-slider-image sow-slider-image-cover" style="background-image: url(<?php echo get_template_directory_uri() ?>/demo/assets/IMG_1315.png);">
+
+			<div class="sow-slider-image-container">
+				<div class="sow-slider-image-wrapper" style="max-width: 720px;">
+					<img width="720" height="500" src="<?php echo get_template_directory_uri() ?>/demo/assets/slider-badge.png" class="attachment-full" alt="slider-badge">
+				</div>
+			</div>
+
+		</li>
+		<li class="sow-slider-image sow-slider-image-cover" style="background-image: url(<?php echo get_template_directory_uri() ?>/demo/assets/IMG_1287.png);">
+
+			<div class="sow-slider-image-container">
+				<div class="sow-slider-image-wrapper" style="max-width: 720px;">
+				<img width="720" height="500" src="<?php echo get_template_directory_uri() ?>/demo/assets/slider-badge.png" class="attachment-full" alt="slider-badge">
+				</div>
+			</div>
+
+		</li>
+
 
 	</ul>
 
 	<ol class="sow-slider-pagination">
 		<li><a href="#" data-goto="0">1</a></li>
 		<li><a href="#" data-goto="1">2</a></li>
+		<li><a href="#" data-goto="2">3</a></li>
+		<li><a href="#" data-goto="3">4</a></li>
+		<li><a href="#" data-goto="4">5</a></li>
 	</ol>
 
 	<div class="sow-slide-nav sow-slide-nav-next">
