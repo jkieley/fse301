826c310be5ba16c174b02bcbba89082dea7048bc
James Kieley <jkieley@asu.edu>
2014-11-30 18:29:19 -0700

    added pics and bootstrap

--- wp-content/themes/influence/header.php
+++ wp-content/themes/influence/header.php
@@ -15,6 +15,7 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/bootstrap.min.css">
 	
 	<?php wp_head(); ?>
 </head>
