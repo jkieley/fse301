c96ff9089c8d2bc1cce64d1e02627f677532154d
James Kieley <jkieley@asu.edu>
2014-11-30 20:33:30 -0700

    add files

--- wp-content/themes/influence/header.php
+++ wp-content/themes/influence/header.php
@@ -16,6 +16,9 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/jquery-ui.css">
+	<script src="<?php echo get_template_directory_uri() ?>/js/jquery-1.11.1.min.js"></script>
+	<script src="<?php echo get_template_directory_uri() ?>/js/jquery-ui.min.js"></script>
 	
 	<?php wp_head(); ?>
 </head>
